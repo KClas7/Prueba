45c8b05a3a0325086a404d10137a1f61bf039f90
KClas7 <kevin.vaca2016@uteq.edu.ec>
2023-11-19 00:11:34 -0500

    ImplementMaximizedWindows

--- OpenEdgeAuto.py
+++ OpenEdgeAuto.py
@@ -1,17 +1,22 @@
 import os
 import time
 from selenium import webdriver
+from selenium.webdriver.edge.service import Service
 from selenium.common.exceptions import NoSuchWindowException
 
 def clear_cache():
     os.system('powershell "Get-Process MicrosoftEdge* | Stop-Process"')
 
 def open_browser(url):
+    # Crea una instancia de EdgeOptions
+    options = webdriver.EdgeOptions()
+    # Añade el argumento para iniciar maximizado
+    options.add_argument("--start-maximized")
     # Asegúrate de tener el driver de Edge instalado y proporciona la ruta al ejecutable de Edge
-    return webdriver.Edge(executable_path='C:\\Users\\ClasK\\3D Objects\\DriversBrowsers\\msedgedriver.exe')
+    s = Service('C:\\Users\\ClasK\\3D Objects\\DriversBrowsers\\msedgedriver.exe')
+    return webdriver.Edge(service=s, options=options)
 
 def main():
-    
     url = 'https://www.bing.com'
     browser = open_browser(url)
     browser.get(url)
@@ -19,7 +24,7 @@ def main():
         try:
             # Intenta obtener las ventanas del navegador
             _ = browser.window_handles
-        except NoSuchWindowException:
+        except NoSuchWindowException:  # Ahora Python sabe qué es NoSuchWindowException
             # Si falla, entonces el navegador ha sido cerrado
             print("El navegador ha sido cerrado")
             clear_cache()
